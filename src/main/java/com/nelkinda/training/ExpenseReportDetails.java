b50aba361da32514911b1ae9ba6de254a75f6ad9
Sadek90 <mohamed.sadek@maxab.io>
2025-12-29 22:30:13 +0200

    refactor: Adds ExpenseReportDetails

---
+++ src/main/java/com/nelkinda/training/ExpenseReportDetails.java
@@ -0,0 +1,14 @@
+package com.nelkinda.training;
+
+public class ExpenseReportDetails {
+    private final int total;
+
+    public ExpenseReportDetails(int total){
+        this.total = total;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+}
