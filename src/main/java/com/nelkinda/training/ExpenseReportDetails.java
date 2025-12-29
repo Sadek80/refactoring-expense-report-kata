047fa6b1529e5285cf3b7ba2e742b89cdf7cf175
Sadek90 <mohamed.sadek@maxab.io>
2025-12-29 22:32:30 +0200

    refactor: Add mealExpenses to ExpenseReportDetails

--- src/main/java/com/nelkinda/training/ExpenseReportDetails.java
+++ src/main/java/com/nelkinda/training/ExpenseReportDetails.java
@@ -2,13 +2,18 @@ package com.nelkinda.training;
 
 public class ExpenseReportDetails {
     private final int total;
+    private final int mealExpenses;
 
-    public ExpenseReportDetails(int total){
+    public ExpenseReportDetails(int total, int mealExpenses){
         this.total = total;
+        this.mealExpenses = mealExpenses;
     }
 
     public int getTotal() {
         return total;
     }
 
+    public int getMealExpenses() {
+        return mealExpenses;
+    }
 }
