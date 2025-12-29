e97696c77aa338db031cc8660b3ff4a825b5bdd8
Sadek90 <mohamed.sadek@maxab.io>
2025-12-29 22:09:25 +0200

    refactor: adds name to ExpenseType

--- src/main/java/com/nelkinda/training/ExpenseType.java
+++ src/main/java/com/nelkinda/training/ExpenseType.java
@@ -1,5 +1,17 @@
 package com.nelkinda.training;
 
 enum ExpenseType {
-    DINNER, BREAKFAST, CAR_RENTAL
+    DINNER("Dinner"),
+    BREAKFAST("Breakfast"),
+    CAR_RENTAL("Car Rental");
+
+    private final String name;
+
+    ExpenseType(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
